ed52c7b729627229a0ff9ca6f168ffdc590edb85
virendrasinh2530 <virubha2530@gmail.com>
2023-10-05 11:24:01 +0530

    add
    add

---
+++ my allen/index.js
@@ -0,0 +1,148 @@
+function all1(){
+    
+
+    document.getElementById("all-1").style.display = "block"; 
+    document.getElementById("men-1").style.display = "none"; 
+    document.getElementById("women-1").style.display = "none";
+    document.getElementById("boys-1").style.display = "none";
+    document.getElementById("girls-1").style.display = "none";
+
+    document.getElementById("all1").style.background = " #e74c3c";
+    document.getElementById("all1").style.borderRadius = "10px";
+    document.getElementById("men1").style.background = "none";
+    document.getElementById("women1").style.background = "none";
+    document.getElementById("boys1").style.background = "none";
+    document.getElementById("girls1").style.background = "none";
+}
+function men1(){
+    document.getElementById("all-1").style.display = "none"; 
+    document.getElementById("men-1").style.display = "block"; 
+    document.getElementById("women-1").style.display = "none";
+    document.getElementById("boys-1").style.display = "none";
+    document.getElementById("girls-1").style.display = "none";
+
+    document.getElementById("men1").style.background = " #e74c3c";
+    document.getElementById("men1").style.borderRadius = "10px";
+    document.getElementById("all1").style.background = "none";
+    document.getElementById("women1").style.background = "none";
+    document.getElementById("boys1").style.background = "none";
+    document.getElementById("girls1").style.background = "none";
+}
+function women1(){
+    document.getElementById("all-1").style.display = "none"; 
+    document.getElementById("men-1").style.display = "none"; 
+    document.getElementById("women-1").style.display = "block";
+    document.getElementById("boys-1").style.display = "none";
+    document.getElementById("girls-1").style.display = "none";
+
+    document.getElementById("women1").style.background = " #e74c3c";
+    document.getElementById("women1").style.borderRadius = "10px";
+    document.getElementById("men1").style.background = "none";
+    document.getElementById("all1").style.background = "none";
+    document.getElementById("boys1").style.background = "none";
+    document.getElementById("girls1").style.background = "none";
+}
+function boys1(){
+    document.getElementById("all-1").style.display = "none"; 
+    document.getElementById("men-1").style.display = "none"; 
+    document.getElementById("women-1").style.display = "none";
+    document.getElementById("boys-1").style.display = "block";
+    document.getElementById("girls-1").style.display = "none";
+
+    document.getElementById("boys1").style.background = " #e74c3c";
+    document.getElementById("boys1").style.borderRadius = "10px";
+    document.getElementById("all1").style.background = "none";
+    document.getElementById("men1").style.background = "none";
+    document.getElementById("women1").style.background = "none";
+    document.getElementById("girls1").style.background = "none";
+}
+function girls1(){
+    document.getElementById("all-1").style.display = "none"; 
+    document.getElementById("men-1").style.display = "none"; 
+    document.getElementById("women-1").style.display = "none";
+    document.getElementById("boys-1").style.display = "none";
+    document.getElementById("girls-1").style.display = "block";
+
+    document.getElementById("girls1").style.background = " #e74c3c";
+    document.getElementById("girls1").style.borderRadius = "10px";
+    document.getElementById("all1").style.background = "none";
+    document.getElementById("men1").style.background = "none";
+    document.getElementById("women1").style.background = "none";
+    document.getElementById("boys1").style.background = "none";
+}
+
+
+function all2(){
+    
+
+    document.getElementById("allitem").style.display = "block"; 
+    document.getElementById("menitem").style.display = "none"; 
+    document.getElementById("womenitem").style.display = "none";
+    document.getElementById("boysitem").style.display = "none";
+    document.getElementById("girlsitem").style.display = "none";
+
+    document.getElementById("all2").style.background = " #e74c3c";
+    document.getElementById("all2").style.borderRadius = "10px";
+    document.getElementById("men2").style.background = "none";
+    document.getElementById("women2").style.background = "none";
+    document.getElementById("boys2").style.background = "none";
+    document.getElementById("girls2").style.background = "none";
+}
+function men2(){
+    document.getElementById("allitem").style.display = "none"; 
+    document.getElementById("menitem").style.display = "block"; 
+    document.getElementById("womenitem").style.display = "none";
+    document.getElementById("boysitem").style.display = "none";
+    document.getElementById("girlsitem").style.display = "none";
+
+    document.getElementById("men2").style.background = " #e74c3c";
+    document.getElementById("men2").style.borderRadius = "10px";
+    document.getElementById("all2").style.background = "none";
+    document.getElementById("women2").style.background = "none";
+    document.getElementById("boys2").style.background = "none";
+    document.getElementById("girls2").style.background = "none";
+}
+function women2(){
+    document.getElementById("allitem").style.display = "none"; 
+    document.getElementById("menitem").style.display = "none"; 
+    document.getElementById("womenitem").style.display = "block";
+    document.getElementById("boysitem").style.display = "none";
+    document.getElementById("girlsitem").style.display = "none";
+
+    document.getElementById("women2").style.background = " #e74c3c";
+    document.getElementById("women2").style.borderRadius = "10px";
+    document.getElementById("men2").style.background = "none";
+    document.getElementById("all2").style.background = "none";
+    document.getElementById("boys2").style.background = "none";
+    document.getElementById("girls2").style.background = "none";
+}
+function boys2(){
+    document.getElementById("allitem").style.display = "none"; 
+    document.getElementById("menitem").style.display = "none"; 
+    document.getElementById("womenitem").style.display = "none";
+    document.getElementById("boysitem").style.display = "block";
+    document.getElementById("girlsitem").style.display = "none";
+
+    document.getElementById("boys2").style.background = " #e74c3c";
+    document.getElementById("boys2").style.borderRadius = "10px";
+    document.getElementById("all2").style.background = "none";
+    document.getElementById("men2").style.background = "none";
+    document.getElementById("women2").style.background = "none";
+    document.getElementById("girls2").style.background = "none";
+}
+function girls2(){
+    document.getElementById("allitem").style.display = "none"; 
+    document.getElementById("menitem").style.display = "none"; 
+    document.getElementById("womenitem").style.display = "none";
+    document.getElementById("boysitem").style.display = "none";
+    document.getElementById("girlsitem").style.display = "block";
+
+    document.getElementById("girls2").style.background = " #e74c3c";
+    document.getElementById("girls2").style.borderRadius = "10px";
+    document.getElementById("all2").style.background = "none";
+    document.getElementById("men2").style.background = "none";
+    document.getElementById("women2").style.background = "none";
+    document.getElementById("boys2").style.background = "none";
+}
+
+
